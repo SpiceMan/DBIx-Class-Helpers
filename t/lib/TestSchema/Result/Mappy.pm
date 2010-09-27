312b2ce2d711bf1a775590add47e538d11e3bc4e
Marcel Montes <spiceman@gmail.com>
2010-09-27 09:48:29 +0900

    remove Inflate::ColumnTime from test dependencies

--- t/lib/TestSchema/Result/Mappy.pm
+++ t/lib/TestSchema/Result/Mappy.pm
@@ -1,22 +1,37 @@
+{
+    package FooDT;
+
+    sub new { bless {}, shift; }
+    sub hms { "22:22:22" }
+    sub ymd { "1111-11-11" }
+}
+
 package TestSchema::Result::Mappy;
 
 use DBIx::Class::Candy
-   -components => ['Helper::Row::ToJSON', 'InflateColumn::DateTime'];
+   -components => ['Helper::Row::ToJSON'];
 
 __PACKAGE__->mk_classdata(
     mapper => {
-        DateTime => sub { my $dt = shift; $dt->hms . ' '. $dt->ymd() },
+        FooDT => sub { my $dt = shift; $dt->hms . ' ' . $dt->ymd },
     },
 );
 
 table 'Mappy';
 
 column 'id';
-column 'somedate' => {
-    data_type => 'date',
-};
+column 'somedate';
 
 primary_key 'id';
 
+sub get_column {
+    my $self = shift;
+    my ($f) = @_;
+
+    if( $f eq "somedate" ) {
+        return FooDT->new;
+    }
+    $self->next::method(@_);
+}
 
 1;
